a043af14f8a7020072089c6491a1ee510553876e
Daria Kriukova <daria.kriukova@elfys.fi>
2024-07-06 08:35:10 +0300

    1. Adding list_files_in_prefix method for listing files in the S3 bucket with a given prefix. 2. Adding linters. 3. Correcteing boto3 Session parameters. 4. Unittest for s3

--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -1,9 +1,21 @@
-"""Connector and methods accessing S3"""
+"""
+Module for interacting with S3 buckets using boto3.
+
+This module provides the S3BucketConnector class, which allows for listing
+files in an S3 bucket with a given prefix. It uses the boto3 library to
+interface with AWS S3 and reads AWS credentials from environment variables.
+
+Usage:
+    from s3_connector import S3BucketConnector
+
+    s3_connector = S3BucketConnector(bucket='my-bucket', endpoint_url='https://s3.amazonaws.com')
+    files = s3_connector.list_files_in_prefix(prefix='my-prefix/')
+"""
 
 import os
+import logging
 import boto3
 from dotenv import load_dotenv
-import logging
 
 load_dotenv()
 
@@ -13,17 +25,13 @@ class S3BucketConnector:
     Class for interacting with S3 buckets
     """
 
-    def __init__(
-        self, access_key: str, secret_key: str, bucket: str, endpoint_url: str
-    ):
+    def __init__(self, bucket: str, endpoint_url: str = None):
         """
         Initialize the S3BucketConnector object.
 
         Parameters:
-        access_key (str): AWS access key.
-        secret_key (str): AWS secret key.
         bucket (str): Name of the S3 bucket.
-        end_point_url (str): URL of the S3 endpoint.
+        endpoint_url (str): URL of the S3 endpoint.
         """
         self._logger = logging.getLogger(__name__)
         self.endpoint_url = endpoint_url
@@ -33,3 +41,16 @@ class S3BucketConnector:
         )
         self._s3 = self.session.resource(service_name="s3", endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
+
+    def list_files_in_prefix(self, prefix: str):
+        """
+        List files in the S3 bucket with a given prefix.
+
+        Parameters:
+        prefix (str): Prefix for the files to list.
+
+        Returns:
+        files: List of file names in the bucket with the given prefix.
+        """
+        files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
+        return files
